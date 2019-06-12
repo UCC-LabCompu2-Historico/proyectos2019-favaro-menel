380f33b9b96f6c86208d32e562ced21406e27f0a
AngeloMenel98 <angelomenel@hotmail.com>
2019-06-12 09:51:58 -0300

    lineas en canvas

--- misFunciones.js
+++ misFunciones.js
@@ -25,3 +25,19 @@ function mostrar_ocultar(id_elem) {
         document.getElementById("tresRes").style.display='block';
     }
 }
+function dibujargrilla() {
+    var canvas=document.getElementById("mycanvas");
+    var ctx=canvas.getContext("2d");
+    ctx.fillStyle="#000";
+    ctx.beginPath();
+    ctx.moveTo(0,canvas.height/2);
+    ctx.lineTo(canvas.width, canvas.height/2);
+    ctx.moveTo(canvas.width/2,0);
+    ctx.lineTo(canvas.width/2,canvas.height);
+    ctx.stroke();
+    ctx.fillStyle="#9c9c9c";
+    ctx.beginPath();
+    ctx.moveTo(10,10);
+    ctx.lineTo(50,50);
+    ctx.closePath();
+}
